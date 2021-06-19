8793816769c654e99c4b0d58a7d44f48a0442560
Paulo Júnio de Souza Nascimento <paulobg8@gmail.com>
2021-06-19 20:34:47 -0300

    Criando o projeto primeiro curso

---
+++ primeiroCurso/src/main/java/com/primeriocurso/primeiroCurso/PrimeiroCursoApplication.java
@@ -0,0 +1,13 @@
+package com.primeriocurso.primeiroCurso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeiroCursoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimeiroCursoApplication.class, args);
+	}
+
+}
---
+++ primeiroCurso/src/test/java/com/primeriocurso/primeiroCurso/PrimeiroCursoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.primeriocurso.primeiroCurso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimeiroCursoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
