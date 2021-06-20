e8f82adcb3db26c0c1d47592002df2756d2a8d82
Paulo Júnio de Souza Nascimento <paulobg8@gmail.com>
2021-06-19 21:18:26 -0300

    pronto para iniciar os estudos.

---
+++ primeiro-projeto/src/main/java/spring/primeiroprojeto/PrimeiroProjetoApplication.java
@@ -0,0 +1,14 @@
+package spring.primeiroprojeto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@EnableAutoConfiguration
+public class PrimeiroProjetoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimeiroProjetoApplication.class, args);
+	}
+
+}
