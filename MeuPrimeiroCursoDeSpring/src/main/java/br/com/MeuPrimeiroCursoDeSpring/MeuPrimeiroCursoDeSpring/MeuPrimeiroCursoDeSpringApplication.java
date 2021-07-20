20b489ac4aa931c58af4d0f09a1ff6dfd1f8df83
Paulo-dev-98 <paulobg8@gmail.com>
2021-07-20 17:14:11 -0300

    Commit de teste

--- MeuPrimeiroCursoDeSpring/src/main/java/br/com/MeuPrimeiroCursoDeSpring/MeuPrimeiroCursoDeSpring/MeuPrimeiroCursoDeSpringApplication.java
+++ MeuPrimeiroCursoDeSpring/src/main/java/br/com/MeuPrimeiroCursoDeSpring/MeuPrimeiroCursoDeSpring/MeuPrimeiroCursoDeSpringApplication.java
@@ -9,5 +9,7 @@ public class MeuPrimeiroCursoDeSpringApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MeuPrimeiroCursoDeSpringApplication.class, args);
 	}
+	
+	//testando commit
 
 }
