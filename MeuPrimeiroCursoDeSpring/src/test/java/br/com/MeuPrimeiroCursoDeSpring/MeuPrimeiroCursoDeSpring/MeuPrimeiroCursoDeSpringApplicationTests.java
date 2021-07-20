03de25aebf97f75601c3bf61dd13d51c12b05d14
Paulo-dev-98 <paulobg8@gmail.com>
2021-07-20 17:03:16 -0300

    Adicionando projeto de curso

---
+++ MeuPrimeiroCursoDeSpring/src/test/java/br/com/MeuPrimeiroCursoDeSpring/MeuPrimeiroCursoDeSpring/MeuPrimeiroCursoDeSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.MeuPrimeiroCursoDeSpring.MeuPrimeiroCursoDeSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeuPrimeiroCursoDeSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
